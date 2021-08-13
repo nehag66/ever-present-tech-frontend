2c8da934eede345fe2a19d3c81c5be87774fb764
Neha Goel <nehagoel66@gmail.com>
2021-08-13 16:14:06 +0530

    login

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = (props) => {
-  return (
-    <div>
-      <div className="container">
-        <div className="logo">Logo</div>
-        <div className="form">
-          <label className="login-head">Welcome</label>
-          <br />
-          <p>As per our records, your details are as under:</p>
-          <label>Name</label>
-          <input type="text" required />
-          <br />
-          <label>Username</label>
-          <input type="text" required />
-          <br />
-          <label>Password</label>
-          <input type="password" required />
-          <br />
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/getClients")
+      .then((response) => response.json())
+      .then((data) => console.log(data.clients));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="logo">Logo</div>
+          <div className="form">
+            <label className="login-head">Welcome</label>
+            <br />
+            <p>As per our records, your details are as under:</p>
+            <label>Name</label>
+            <input type="text" required />
+            <br />
+            <label>Username</label>
+            <input type="text" required />
+            <br />
+            <label>Password</label>
+            <input type="password" required />
+            <br />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
--- src/container/Login/Login.js
+++ src/container/Login/Login.js
@@ -1,57 +1,10 @@
 import React, { Component } from "react";
 import "./Login.css";
-import Button from "../../components/Button/Button";
-// import { connect } from "react-redux";
+import { Route } from "react-router-dom";
+
+import { connect } from "react-redux";
 // // import { addName } from "../../action";
 // // import { actionCreators } from "../../action";
-
-// import React from "react";
-
-// const Login = (props) => {
-//   fetch("http://localhost:8080/getClients")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-
-//   //     componentDidMount() {
-//   //     document.title = `You clicked ${data} times`;
-//   //   }
-//   return (
-//     <div>
-//       {/* <button onClick={() => props.addName("Chris")}>button</button>
-//       {props.names.map((name) => {
-//         return <div>{name}</div>;
-//       })} */}
-//       <div className="container">
-//         <div className="logo">Logo</div>
-//         <div className="form">
-//           <label className="login-head">Login form</label>
-//           <br />
-//           <label>Username</label>
-//           <input
-//             type="text"
-//             name="username"
-//             id="username"
-//             // value=""
-//             // onChange={(e) => actionCreators.updateUserName(e.target.value)}
-//             required
-//           />
-//           <br />
-//           <label>Password</label>
-//           <input
-//             type="password"
-//             name="password"
-//             id="password"
-//             // value=""
-//             // onChange={(e) => actionCreators.updatePassword(e.target.value)}
-//             required
-//           />
-//           <br />
-//           <Button />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 // // const mapStateToProps = (state) => {
 // //   return {
@@ -61,20 +14,26 @@ import Button from "../../components/Button/Button";
 // // export default connect(mapStateToProps, { updateUserName, updatePassword })(
 // //   Login
 // // );
-// export default Login;
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { username: "", password: "" };
+    this.handleStateChange = this.handleStateChange.bind(this);
   }
-
-  fetchData;
 
   componentDidMount() {
     fetch("http://localhost:8080/getClients")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => console.log(data.clients));
+  }
+  handleStateChange(state) {
+    console.log("clicked");
+    this.setState({
+      username: "neha",
+      password: "neha",
+    });
+    console.log(this.username, this.password);
   }
 
   //   componentDidUpdate() {
@@ -111,7 +70,20 @@ class Login extends Component {
               required
             />
             <br />
-            <Button />
+            <Route
+              render={({ history }) => (
+                <button
+                  className="btn"
+                  type="button"
+                  onClick={() => {
+                    this.handleStateChange();
+                    history.push("/home");
+                  }}
+                >
+                  Login
+                </button>
+              )}
+            />
           </div>
         </div>
       </div>
